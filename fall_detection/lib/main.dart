949d45978ebed2596e6733cd5a468a171f010d62
Jaswanth Beere <jaswanthbeere@gmail.com>
2023-10-11 17:19:59 +0530

    add scroll to login screen

--- fall_detection/lib/main.dart
+++ fall_detection/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Fall Detection App',
       theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
       home: WelcomeScreen(),
     );
   }
--- fall_detection/lib/screens/login_screen.dart
+++ fall_detection/lib/screens/login_screen.dart
@@ -17,52 +17,55 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Login')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Image.network(
-                "https://cdn-icons-png.flaticon.com/512/295/295128.png"),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_usernameController.text == 'admin' &&
-                    _passwordController.text == '12345') {
-                  Navigator.of(context).pushReplacement(MaterialPageRoute(
-                      builder: (context) => const NotificationList()));
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Invalid login credentials')),
-                  );
-                }
-              },
-              child: const NormalText(
-                text: 'Login',
-                color: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-            ),
-          ],
+              Image.network(
+                  "https://cdn-icons-png.flaticon.com/512/295/295128.png"),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _usernameController,
+                decoration: const InputDecoration(labelText: 'Username'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_usernameController.text == 'admin' &&
+                      _passwordController.text == '12345') {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => const NotificationList()));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                          content: Text('Invalid login credentials')),
+                    );
+                  }
+                },
+                child: const NormalText(
+                  text: 'Login',
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
